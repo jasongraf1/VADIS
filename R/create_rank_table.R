fa95dee0bded61699a5e9a2fe2d3acfdc54c90bb
Jason Grafmiller (English Language and Linguistics) <j.grafmiller@bham.ac.uk>
2020-08-04 09:47:47 +0100

    update variable importance calculations for `party` RFs to use `permimp()`

--- R/create_rank_table.R
+++ R/create_rank_table.R
@@ -6,10 +6,15 @@
 #'
 #' @author Jason Grafmiller
 #'
-#' @details The function loops through a list of random forest objects, extracts the variable importance estimates, and compiles them in a single dataframe.
+#' @details The function loops through a list of random forest objects, extracts the variable importance estimates, and compiles them in a single dataframe. For \code{\link[party]{RandomForest-class}} models, function uses the \code{\link[permimp]{permimp}} function (Debeer & Strobl 2020) with the default threshold (0.95) and \code{AUC= TRUE} (Janitza et al. 2013).
 #' For forests fit with \code{\link[ranger]{ranger}} or \code{\link[randomForest]{randomForest}}, the \code{importance} argument must be specified.
 #' #'
 #' @return A dataframe
+#'
+#' @references
+#' Debeer, Dries & Carolin Strobl. 2020. Conditional permutation importance revisited. *BMC Bioinformatics* 21(1). 307. https://doi.org/10.1186/s12859-020-03622-2.
+#'
+#' Janitza, Silke, Carolin Strobl & Anne-Laure Boulesteix. 2013. An AUC-based permutation variable importance measure for random forests. *BMC Bioinformatics* 14(1). 119. https://doi.org/10.1186/1471-2105-14-119.
 #'
 #' @export
 #'
@@ -33,8 +38,15 @@ create_rank_table <- function(mod_list, conditional = FALSE) {
     if (conditional){
       cat(paste("Computing varimpAUC() for", length(mod_list), "models. This may take some time...\nIf it takes too long, consider setting conditional = FALSE."))
     } else cat(paste("Computing varimpAUC() for", length(mod_list), "models. This may take some time..."))
-    varimp_list <- lapply(mod_list, FUN = function(m) party::varimpAUC(m, conditional = conditional))
-    varimp_tab <- data.frame(sapply(varimp_list, c))
+    varimp_list <- lapply(
+      mod_list,
+      FUN = function(m) {
+        permimp::permimp(m, conditional = conditional, AUC = TRUE,
+                         progressBar = FALSE)
+        })
+    varimp_tab <- as.data.frame(
+      do.call("cbind", lapply(varimp_list, function(x) x$values))
+    )
     names(varimp_tab) <- names(mod_list)
   } else if (type == "randomForest"){
     varimp_list <- lapply(mod_list, FUN = function(m) randomForest::importance(m))
